25ee095e85ab4186dab6a9b040daa89eb3441048
Christian Boyle <mail@christianboyle.com>
2021-05-08 10:44:07 -0500

    Add with-apollo-client example

---
+++ with-apollo-client/src/App/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const FILMS_QUERY = gql`
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
+
+export default function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
+
+  return (
+    <div>
+      <h1>SpaceX Launches</h1>
+      <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ with-apollo-client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  rootElement
+);
