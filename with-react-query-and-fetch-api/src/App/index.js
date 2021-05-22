94b707652b368f79f867f43ecff4958fbdbce51a
Christian Boyle <mail@christianboyle.com>
2021-05-22 06:55:10 -0500

    Add with-react-query-and-fetch-api example

---
+++ with-react-query-and-fetch-api/src/App/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+const endpoint = "https://api.spacex.land/graphql/";
+const FILMS_QUERY = `
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
+
+export default function App() {
+  const { data, isLoading, error } = useQuery("launches", () => {
+    return fetch(endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: FILMS_QUERY })
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Error fetching data");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => data.data);
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
+  return (
+    <div>
+      <h1>SpaceX Launches</h1>
+      <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
